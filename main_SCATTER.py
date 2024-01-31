7d63eac5c4d4f784b26718e1cf0bc952deba74ae
alber <trfytfty>
2024-01-31 15:56:12 +0100

    try push and update

--- main_SCATTER.py
+++ main_SCATTER.py
@@ -228,7 +228,7 @@ def main():
                     lab_classes=lab_classes)  # function to compute the metrics (accuracy and confusion matrix)
 
     plot_kernels(J,L,scattering)
-    #TERRA CHIAMA RIGHE
+    #CHATGPT CHIAMA CHIARA
 
 if __name__ == "__main__":
     main()
