6fd1db51920b048ad3437433f46483bb7048226e
VirginiaFilippi <virginia.filippi@studenti.univr.it>
2024-02-08 16:34:37 +0100

    corrected scatnet architecture

--- ScatNet.py
+++ ScatNet.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.optim
 from kymatio.torch import Scattering2D
 
@@ -9,31 +9,28 @@ ScatNet 2D
 
 
 class ScatNet2D(torch.nn.Module):
-    def __init__(self, input_channels: int, scattering: Scattering2D):
+    def __init__(self, input_channels: int, scattering: Scattering2D, num_classes: int):
         super(ScatNet2D, self).__init__()
 
         self.input_channels = input_channels
         self.scattering = scattering
+        self.num_classes = num_classes
 
         # linear with size [K*25*25, 1024],
         # linear with size [1024, 1024],
         # linear with size [1024,3]
         # All the linear layer are followed by ReLU activation
+        # 32: grandezza immagine dopo scattering
+        self.lin = torch.nn.Linear(self.input_channels * 32 * 32 * 3, 576)
 
         self.classifier = torch.nn.Sequential(
-            # torch.nn.Linear((self.input_channels * 25 * 25, 1024),
-            torch.nn.Linear(248832, 1024),
+            torch.nn.Linear(576, 256),
             torch.nn.ReLU(),
-            torch.nn.Linear(1024, 1024),
+            torch.nn.Linear(256, 32),
             torch.nn.ReLU(),
-            torch.nn.Linear(1024, 2),
-            torch.nn.ReLU()
+            torch.nn.Dropout(p=0.1),
+            torch.nn.Linear(32, self.num_classes)
         )
-
-        # self.fc1 = nn.Linear(576,256)
-        # self.drop = nn.Dropout(p=0.1)
-        # self.fc2 = nn.Linear(256,32)
-        # self.fc3 = nn.Linear(32,self.num_classes)
 
         # Weights initialization
         for layer in self.classifier:
@@ -43,6 +40,7 @@ class ScatNet2D(torch.nn.Module):
     def forward(self, x):
         x = self.scattering(x)  # scattering transform
         x = x.view(x.size(0), -1)
+        x = self.lin(x)
         x = self.classifier(x)  
         
         # x = F.relu(self.fc1(x))
